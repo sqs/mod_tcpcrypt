854b539f60ddd195a379338d33577b0be6cb85e2
Quinn Slack <qslack@qslack.com>
2010-08-02 13:16:09 -0700

    default vals = <none>

--- test/tcpcrypt.sh
+++ test/tcpcrypt.sh
@@ -2,6 +2,6 @@
 
 echo Content-Type: text/plain
 echo
-echo REMOTE_ADDR = ${REMOTE_ADDR:='unknown'}
-echo TCP_CRYPT_ENABLE = ${TCP_CRYPT_ENABLE:='unknown'}
-echo TCP_CRYPT_SESSID = ${TCP_CRYPT_SESSID:='unknown'}
+echo REMOTE_ADDR = ${REMOTE_ADDR}
+echo TCP_CRYPT_ENABLE = ${TCP_CRYPT_ENABLE:='<none>'}
+echo TCP_CRYPT_SESSID = ${TCP_CRYPT_SESSID:='<none>'}

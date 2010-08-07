6b3c7cedf0b9f18698449d22cd1eca30cf2cf2ec
Quinn Slack <sqs@cs.stanford.edu>
2010-08-06 21:24:22 -0700

    php example: getting tcpcrypt env vars

---
+++ test/tcpcrypt.php
@@ -0,0 +1,5 @@
+<?php
+echo "REMOTE_ADDR = " . $_SERVER['REMOTE_ADDR'] . "\n";
+echo "TCP_CRYPT_ENABLE = " . $_SERVER['TCP_CRYPT_ENABLE'] . "\n";
+echo "TCP_CRYPT_SESSID = " . $_SERVER['TCP_CRYPT_SESSID'] . "\n";
+?>

82829118fc5e9f025b32abf4650ddfb58f46da48
Quinn Slack <sqs@cs.stanford.edu>
2010-08-18 01:32:31 -0700

    initial version of test for mod_tcpcrypt

---
+++ test/test_mod_tcpcrypt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+testname=$1
+url=$2
+curlout=/tmp/curlout-$$
+
+
+case $testname in
+    on)
+        echo -n Testing mod_tcpcrypt with server tcpcryptd running...
+        curl --tcp-nodelay --limit-rate 30 $url > $curlout 2>/dev/null &
+        sleep 0.5
+        sessid=$(../tcpcrypt/user/test/tcpcrypt -v -N | tail -n 1 | rev | cut -d ' ' -f 1 | rev)
+        wait
+        grep 'TCP_CRYPT_ENABLE = 1' $curlout && \
+            grep "${sessid/[^\w\d]/}" $curlout && \
+            echo "PASS" && exit 0
+        echo "FAIL"
+        echo Got
+        cat $curlout
+        exit 1
+        ;;
+    off)
+        echo -n Testing mod_tcpcrypt with server tcpcryptd off...
+        curl $url > $curlout 2>/dev/null
+        grep 'TCP_CRYPT_ENABLE = 0' $curlout && \
+            grep "TCP_CRYPT_SESSID = <none>" $curlout && \
+            echo "PASS" && exit 0
+        echo "FAIL"
+        echo got
+        cat $curlout
+        exit 1
+        ;;
+esac
+# TODO: test with server tcpcryptd on, client tcpcryptd off
